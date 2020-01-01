0287eb614d78c485193ada77abcaf0a1cd6b2b48
Andrew Erlanger <a.erlanger@gmail.com>
2020-01-01 16:04:35 +0100

    style contact and about pages

--- main/templatetags/template_tags.py
+++ main/templatetags/template_tags.py
@@ -8,3 +8,14 @@ def preview(text):
     line_list = text.split("\n")[:2]
     line_list.append("...")
     return "\n".join(line_list)
+
+
+@register.filter
+def add_spaces(text):
+    return text.replace("<p>", "<p class='mb-4'>")
+
+
+@register.filter
+def color_navbar_links(page, link):
+    if link in page:
+        return "text-indigo-600"
