f6b5fa51e17508d3cef6fa979ef39f196e64b4b4
Andrew Erlanger <a.erlanger@gmail.com>
2023-01-03 11:49:56 +1100

    wip

--- jenny_erlanger/settings.py
+++ jenny_erlanger/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+import dj_database_url
 from cloudinary import config as cloudinary_config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -85,6 +85,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -148,6 +151,3 @@ WEBPACK_LOADER = {
         'IGNORE': ['.+\.hot-update.js', '.+\.map']
     }
 }
-
-# Heroku DB
-django_heroku.settings(locals())
